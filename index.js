7266c4fe7a765a6de5f22313425a12ee1f86f9c6
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-12-04 04:42:32 +0530

    Operator arithmetic

---
+++ index.js
@@ -0,0 +1,86 @@
+// This repo is all about Javascript handson and Skill Development
+
+// let sid=6
+// sid=5
+// let sid=7
+// console.log(sid)
+
+/*
+var vishen=6
+vishen=10
+var vishen="Rajat"
+console.log(vishen)
+
+var shahi=45
+function myfucn(){
+    console.log(shahi);
+    console.log("Siddharth");
+    var shahi=55;
+    console.log(shahi)
+}
+
+myfucn()
+
+let shahis=45
+function myfucns(){
+    console.log(shahis)
+    console.log("Siddharth");
+    
+}
+
+myfucns()
+
+let gopi=45
+
+function gopis(){
+    console.log(gopi)
+    console.log("gopi")
+}
+
+gopis()
+
+
+
+var sid=25
+function siddharth(){
+    console.log(sid)
+}
+
+siddharth()
+
+
+
+var x=21
+var girls=function () {
+    console.log(x)
+    var x=20
+}
+
+girls()
+
+*/
+
+
+const sid=10
+function vishen(){
+    console.log(sid)
+    const shahi=10
+    console.log("vishen function")
+    console.log(shahi)
+}
+
+vishen()
+
+// console.log(shahi)
+console.log(sid)
+
+let a =10;
+let b=20;
+console.log("10 + 20 = ",a+b)
+let c=a+b
+console.log(c+=4)
+console.log(c)
+
+
+
+
