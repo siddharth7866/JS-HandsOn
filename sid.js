32578cb8967fe4afeb70facb945e0a26003f668f
supriya pandey <supriyapandey769@gmail.com>
2023-12-28 14:22:31 +0530

    try catch bock

--- sid.js
+++ sid.js
@@ -7,10 +7,24 @@
 
 // if(moviesPass) console.log('I can watch movie')
 
-let sid=[1,2,4,4,5,6,7,8,9]
-let result=sid.forEach((x)=>console.log(x+5))
-console.log(result)
-let filterr=sid.filter(x=>x===4)
-console.log(filterr)
-let reducee=sid.reduce((x,y,z)=>console.log(z+y))
-console.log(reducee)
+// let sid=[1,2,3,4]
+// let result=sid.forEach((x)=>console.log(x+5))
+// console.log(result)
+// let filterr=sid.filter(x=>x===4)
+// console.log(filterr)
+// let reducee=sid.reduce((x,y)=>(x+y))
+// console.log(reducee)
+
+// try {
+//     // let a =10
+//     console.log(a)
+// } catch (sid) {
+//     console.log(sid)
+//     console.log("I am in catch block")
+// }finally{
+//     console.log("I am in finally block")
+// }
+
+// var letters='abc'
+// console.log(letters.match(/s/))
+
